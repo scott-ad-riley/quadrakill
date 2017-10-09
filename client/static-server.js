c90c00ec255e8a101e7f8b1cf72b5eee3edda82d
Scott Riley <scott.riley111@gmail.com>
2017-10-09 16:51:34 +0100

    removes redundant var

--- client/static-server.js
+++ client/static-server.js
@@ -10,7 +10,6 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
   var port = server.address().port;
 
   console.log('Client Server started at http://localhost:%s', port);

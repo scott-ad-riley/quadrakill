609e52aa4b05daa1c72422692744733533310902
Scott Riley <scott.riley111@gmail.com>
2017-10-15 10:29:53 +0100

    Adds the travis deployment script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ssh -v travis@138.68.142.81 'echo worked >> ~/testing'
+$TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d server_dist
+rsync -avzqr --delete-after $TRAVIS_BUILD_DIR travis@138.68.142.81:/var/www/crossfire.placeofthin.gs

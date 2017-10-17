505b5aa722f8f626a2cae93160e0b36ac10cba7d
Scott Riley <scott.riley111@gmail.com>
2017-10-17 22:44:45 +0100

    restarts socket server when deployed

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 ssh -v travis@138.68.142.81 'echo worked >> ~/testing'
 $TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d server_dist
 rsync -avzqr --delete-after $TRAVIS_BUILD_DIR travis@138.68.142.81:/var/www/quadrakill.placeofthin.gs
+ssh travis@138.68.142.81 'pm2 restart socket-server'

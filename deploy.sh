805e95db9b92b5626798813b719efebf21f8739b
Scott Riley <scott.riley111@gmail.com>
2017-10-20 20:56:59 +0100

    debugging travis

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
-ssh -v travis@138.68.142.81 "echo $(git rev-parse HEAD) >> ~/deploys"
-$TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d server_dist
-rsync -avzqr --delete-after $TRAVIS_BUILD_DIR/quadrakill travis@138.68.142.81:~/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)
-ln -sfn ~/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD) /var/www/quadrakill.placeofthin.gs
+$TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d $TRAVIS_BUILD_DIR/server_dist
+ssh travis@138.68.142.81 "echo $(git rev-parse HEAD) >> ~/deploys"
+ssh travis@138.68.142.81 "mkdir /home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)"
+rsync -avzqr --delete-after $TRAVIS_BUILD_DIR/ travis@138.68.142.81:/home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)
+ln -sfn /home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD) /var/www/quadrakill.placeofthin.gs
 ssh travis@138.68.142.81 'pm2 restart socket-server'

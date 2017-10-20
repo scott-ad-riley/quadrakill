29771771cfc4f00d67c05f626b93033c562b4d53
Scott Riley <scott.riley111@gmail.com>
2017-10-20 20:08:27 +0100

    Capistrano-like deployments

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 ssh -v travis@138.68.142.81 "echo $(git rev-parse HEAD) >> ~/deploys"
 $TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d server_dist
-rsync -avzqr --delete-after $TRAVIS_BUILD_DIR travis@138.68.142.81:/var/www/quadrakill.placeofthin.gs
+rsync -avzqr --delete-after $TRAVIS_BUILD_DIR/quadrakill travis@138.68.142.81:~/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)
+ln -sfn ~/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD) /var/www/quadrakill.placeofthin.gs
 ssh travis@138.68.142.81 'pm2 restart socket-server'

9ba1ae5337f2a8ed58469d70d16ce4247af5ed7d
Scott Riley <scott.riley111@gmail.com>
2017-10-20 19:49:35 +0100

    Making some tweaks to travis deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh -v travis@138.68.142.81 'echo worked >> ~/testing'
+ssh -v travis@138.68.142.81 "echo $(git rev-parse HEAD) >> ~/deploys"
 $TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d server_dist
 rsync -avzqr --delete-after $TRAVIS_BUILD_DIR travis@138.68.142.81:/var/www/quadrakill.placeofthin.gs
 ssh travis@138.68.142.81 'pm2 restart socket-server'

d67d7787d83e03dd15e68d2a92ca32440f105c56
Scott Riley <scott.riley111@gmail.com>
2017-10-21 11:21:15 +0100

    Including and encrypting the travis droplet password

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -e
 $TRAVIS_BUILD_DIR/node_modules/.bin/babel server -d $TRAVIS_BUILD_DIR/server_dist
 ssh travis@138.68.142.81 "echo $(git rev-parse HEAD) >> ~/deploys"
 ssh travis@138.68.142.81 "mkdir /home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)"
+
 rsync -avzqr --delete-after $TRAVIS_BUILD_DIR/ travis@138.68.142.81:/home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD)
-ssh travis@138.68.142.81 "ln -sfn /home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD) /var/www/quadrakill"
+echo $DROPLET_TRAVIS_PASSWORD | \
+  ssh travis@138.68.142.81 "sudo -S ln -sfn /home/travis/app/quadrakill.placeofthin.gs/$(git rev-parse HEAD) /var/www/quadrakill"
 ssh travis@138.68.142.81 'pm2 reload socket-server'
